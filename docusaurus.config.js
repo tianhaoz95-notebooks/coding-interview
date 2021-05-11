89707aea56c663f13ca642356af85cec8dbc0d6a
Tianhao Zhou <tianhaoz@umich.edu>
2021-05-11 06:44:14 +0000

    fix: broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,15 +41,6 @@ module.exports = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Getting Started',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
           title: 'Community',
           items: [
             {
